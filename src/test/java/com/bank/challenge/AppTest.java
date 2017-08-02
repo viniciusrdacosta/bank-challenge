c84069aabcaced56a4ef6ecf244e68f1abaca063
Vinicius Costa <viniciusrdacosta@gmail.com>
2017-08-02 09:14:54 -0300

    Project Scaffold

---
+++ src/test/java/com/bank/challenge/AppTest.java
@@ -0,0 +1,17 @@
+package com.bank.challenge;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = App.class)
+public class AppTest {
+
+  @Test
+  public void contextLoads() { }
+
+}
+
+
